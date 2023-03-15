9f815b03a0a46b860c356bfdbba6ef8523214ecd
tangxianglin <txline0420@163.com>
2023-03-15 14:03:00 +0800

    项目初始化

---
+++ src/main/java/com/txl/linkage/EquipmentLinkageApplication.java
@@ -0,0 +1,13 @@
+package com.txl.linkage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLinkageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentLinkageApplication.class, args);
+    }
+
+}
